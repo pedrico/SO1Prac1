2f191d8c0c38cc18407001efcd0e1df6c49e8630
pedrico <pedrico_tay@hotmail.com>
2017-12-12 14:21:45 -0600

    Commit anterior PaginaWeb renderizada por Jade. Commit Actual Chat utilizando sockets de express.

---
+++ app2.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+app.use(express.static(__dirname + '/node_modules'));
+app.set("view engine", "jade");
+app.get('/', function (req, res) {
+  res.sendFile(__dirname+'/views/indexsocket.html');
+});
+
+io.on('connection', function(client) {
+    console.log('Client connected...');
+
+    client.on('join', function(data) {
+        console.log(data);
+        client.emit('messages', 'Hello from server');
+    });
+
+    client.on('messages', function(data) {
+           client.emit('broad', data);
+           client.broadcast.emit('broad',data);
+    });
+    });
+
+
+server.listen(3000);
