ebbf53f27171d4eff266113c7de4d27e3aa84983
pedrico <pedrico_tay@hotmail.com>
2017-12-12 18:44:00 -0600

    Prueba lectura archivo 4

--- app2.js
+++ app2.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
+var exec = require('child_process').exec,child, child1;
 
 
 
