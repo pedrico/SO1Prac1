7c6d133deaf859b3b57e449224b48a21b4f0d522
pedrico <pedrico_tay@hotmail.com>
2017-12-13 21:50:38 -0600

    Prueba lectura archivo 22

--- app2.js
+++ app2.js
@@ -73,169 +73,169 @@ io.on('connection', function(client) {
             for (j = 0; j < idProcesos.length; j++) {
               console.log('Valor j 2: ', j);
 
-try {
-              const readline = require('readline');
-              const fs = require('fs');
+              try {
+                const readline = require('readline');
+                const fs = require('fs');
 
-              var directorio = "/proc/"+idProcesos[j]+"/stat";
-              //console.log('Directorio: ', directorio);
-              (function(varDirectorio){
-              fs.stat(varDirectorio, function(err, stats){
+                var directorio = "/proc/"+idProcesos[j]+"/stat";
+                //console.log('Directorio: ', directorio);
+                (function(varDirectorio){
+                  fs.stat(varDirectorio, function(err, stats){
 
-                console.log('Directorio: ', varDirectorio);
-                if(!(err)){
-                const rl = readline.createInterface({
-                  input: fs.createReadStream(varDirectorio)
-                });
-                  console.log('Directorio: ', varDirectorio);
-                rl.on('line', function(linea){
-                  var spliteada = linea.split(" ")
-                  var primera = spliteada[0];//id
-                  var segunda = spliteada[1];//nombre
-                  var tercera = spliteada[2];//estado
-                  var cuarta = spliteada[3];//Usuario
-                  var quinta = spliteada[23];//memoria
-
-
-
-                      //console.log('Leyendo estado: ', idProcesos[j]);
-                      // var comparacion = stdout.localeCompare("S")
-                      // console.log('Local compare: ' + comparacion);
-                      // console.log('Local compare: ' + stdout.trim()+ "-");
-                      // console.log('Local compare: ' + "S");
-                      if (0 == tercera.trim().localeCompare("S")) {
-                        cantidadsuspendidos ++;
-                      }else if (0 == tercera.trim().localeCompare("R")) {
-                        cantidadejecucion ++;
-                      }else if (0 == tercera.trim().localeCompare("T")) {
-                        cantidaddetenidos ++;
-                      }else if (0 == tercera.trim().localeCompare("Z")) {
-                        cantidadzombies ++;
-                      }
-                      ProcesosId.push(primera);
-                      ProcesosNombre.push(segunda);
-                      ProcesosUsuario.push(cuarta);
-                      ProcesosEstado.push(tercera);
-                      ProcesosRam.push(quinta);
+                    console.log('Directorio: ', varDirectorio);
+                    if(!(err)){
+                      const rl = readline.createInterface({
+                        input: fs.createReadStream(varDirectorio)
+                      });
+                      console.log('Directorio: ', varDirectorio);
+                      rl.on('line', function(linea){
+                        var spliteada = linea.split(" ")
+                        var primera = spliteada[0];//id
+                        var segunda = spliteada[1];//nombre
+                        var tercera = spliteada[2];//estado
+                        var cuarta = spliteada[3];//Usuario
+                        var quinta = spliteada[23];//memoria
 
 
 
+                        //console.log('Leyendo estado: ', idProcesos[j]);
+                        // var comparacion = stdout.localeCompare("S")
+                        // console.log('Local compare: ' + comparacion);
+                        // console.log('Local compare: ' + stdout.trim()+ "-");
+                        // console.log('Local compare: ' + "S");
+                        if (0 == tercera.trim().localeCompare("S")) {
+                          cantidadsuspendidos ++;
+                        }else if (0 == tercera.trim().localeCompare("R")) {
+                          cantidadejecucion ++;
+                        }else if (0 == tercera.trim().localeCompare("T")) {
+                          cantidaddetenidos ++;
+                        }else if (0 == tercera.trim().localeCompare("Z")) {
+                          cantidadzombies ++;
+                        }
+                        ProcesosId.push(primera);
+                        ProcesosNombre.push(segunda);
+                        ProcesosUsuario.push(cuarta);
+                        ProcesosEstado.push(tercera);
+                        ProcesosRam.push(quinta);
 
-                } );
+
+
+
+                      } );
+                    }
+                  });})(directorio)
+
+                } catch (err) {
+                  console.log('exec 3 error: ' + err);
+                }
+
+
               }
-            });})(directorio)
+              var Total = ProcesosId.length;
+              var Totales =  ` <div class="row">
+              <div class="col-sm-3">
+              Total Procesos: `+ Total +`
+              </div>
+              <div class="col-sm-3">
+              En Ejecución: `+ cantidadejecucion +`
+              </div>
+              <div class="col-sm-3">
+              Suspendidos: `+ cantidadsuspendidos +`
+              </div>
+              <div class="col-sm-3">
+              Detenidos: `+ cantidaddetenidos +`
+              </div>
+              <div class="col-sm-3">
+              Zombies:`+ cantidadejecucion +`
+              </div>
 
-            } catch (err) {
-              console.log('exec 3 error: ' + err);
-            }
+              `;
 
-
-            }
-            var Total = cantidadejecucion.length + cantidadsuspendidos.length + cantidaddetenidos.length + cantidadzombies.lenght;
-            var Totales =  ` <div class="row">
-                            <div class="col-sm-3">
-                            Total Procesos: `+ Total +`
-                            </div>
-                            <div class="col-sm-3">
-                            En Ejecución: `+ cantidadejecucion.length +`
-                            </div>
-                            <div class="col-sm-3">
-                            Suspendidos: `+ cantidadsuspendidos.length +`
-                            </div>
-                            <div class="col-sm-3">
-                            Detenidos: `+ cantidaddetenidos.length +`
-                            </div>
-                            <div class="col-sm-3">
-                            Zombies:`+ cantidadejecucion.length +`
-                            </div>
-
-                            `;
-
-            var tabla = Totales + `<table
-            class='table table-hover'>
-                                  <thead>
-                                  <tr>
-                <th scope='col'>Id</th>
-                <th scope='col'>Nombre</th>
-                <th scope='col'>Usuario</th>
-                <th scope='col'>Estado</th>
+              var tabla = Totales + `<table
+              class='table table-hover'>
+              <thead>
+              <tr>
+              <th scope='col'>Id</th>
+              <th scope='col'>Nombre</th>
+              <th scope='col'>Usuario</th>
+              <th scope='col'>Estado</th>
               </tr>
-            </thead>
-            <tbody>`;
-            var estados = "";
-            for (i = 0; i < ProcesosNombre.length; i++) {
-              tabla += `<tr>
-              <td>"` + ProcesosId[i]+`"</td>
-              <td>"` + ProcesosNombre[i]+`"</td>
-              <td>"` + ProcesosUsuario[i]+`"</td>
-              <td>"` + ProcesosEstado[i]+`"</td>
-              <td>"` + ProcesosRam[i]+`"</td>
+              </thead>
+              <tbody>`;
+              var estados = "";
+              for (i = 0; i < ProcesosNombre.length; i++) {
+                tabla += `<tr>
+                <td>"` + ProcesosId[i]+`"</td>
+                <td>"` + ProcesosNombre[i]+`"</td>
+                <td>"` + ProcesosUsuario[i]+`"</td>
+                <td>"` + ProcesosEstado[i]+`"</td>
+                <td>"` + ProcesosRam[i]+`"</td>
 
-              </tr>`;
+                </tr>`;
 
-              estados += ProcesosNombre[i] + "-";
+                estados += ProcesosNombre[i] + "-";
 
-            }
+              }
 
-            tabla += `</tbody>
-            </table>`;
-            // client.emit('contador', "Procesos Suspendidos: "
-            // +cantidadsuspendidos+"<br/>"+
-            // "Procesos en Ejecución: "
-            // +cantidadejecucion+"<br/>"+
-            // "Procesos Detenidos: "
-            // +cantidaddetenidos+"<br/>"+
-            // "Procesos Zombies: "
-            // +cantidadzombies+"<br/>"+
-            // ProcesosNombre.length +" - " + estados + " - ");
+              tabla += `</tbody>
+              </table>`;
+              // client.emit('contador', "Procesos Suspendidos: "
+              // +cantidadsuspendidos+"<br/>"+
+              // "Procesos en Ejecución: "
+              // +cantidadejecucion+"<br/>"+
+              // "Procesos Detenidos: "
+              // +cantidaddetenidos+"<br/>"+
+              // "Procesos Zombies: "
+              // +cantidadzombies+"<br/>"+
+              // ProcesosNombre.length +" - " + estados + " - ");
 
-            client.emit('contador', tabla);
-            ProcesosNombre=[];
-            cantidadsuspendidos =0;
-            cantidadejecucion= 0;
-            cantidaddetenidos = 0;
-            cantidadzombies=0;
-          }.bind(this))
-        }
-      )
-    }
-    ,5000);
+              client.emit('contador', tabla);
+              ProcesosNombre=[];
+              cantidadsuspendidos =0;
+              cantidadejecucion= 0;
+              cantidaddetenidos = 0;
+              cantidadzombies=0;
+            }.bind(this))
+          }
+        )
+      }
+      ,5000);
 
-  });
-
-
-  function aumentar(cont)
-  {
-    cont ++;
-    return cont;
-  }
+    });
 
 
-  function crearArchivo(funLeerArchivo)
-  {
-    this.cantidadProcesos = 0;
-    idProcesos = [];
-    //ProcesosNombre= [];
-    exec("ls /proc > informacion.txt " ,
-    function(error, stdout, stderr){
-      if (error !== null) {
-        console.log('exec 1 error: ' + error);
-      } else {
-        //Llamar a leerArchivo
-        funLeerArchivo(
-          //Sumar Procesos
-          function (line) {
-            var esnum = isNumber(line)
-            if(esnum){
-              this.cantidadProcesos ++;
-              idProcesos.push(line);
-            }
-          }.bind(this));
-        }
-      }.bind(this));
+    function aumentar(cont)
+    {
+      cont ++;
+      return cont;
     }
 
 
-    function isNumber(n) { return !isNaN(parseFloat(n)) && !isNaN(n - 0) }
+    function crearArchivo(funLeerArchivo)
+    {
+      this.cantidadProcesos = 0;
+      idProcesos = [];
+      //ProcesosNombre= [];
+      exec("ls /proc > informacion.txt " ,
+      function(error, stdout, stderr){
+        if (error !== null) {
+          console.log('exec 1 error: ' + error);
+        } else {
+          //Llamar a leerArchivo
+          funLeerArchivo(
+            //Sumar Procesos
+            function (line) {
+              var esnum = isNumber(line)
+              if(esnum){
+                this.cantidadProcesos ++;
+                idProcesos.push(line);
+              }
+            }.bind(this));
+          }
+        }.bind(this));
+      }
 
-    server.listen(3000);
+
+      function isNumber(n) { return !isNaN(parseFloat(n)) && !isNaN(n - 0) }
+
+      server.listen(3000);
